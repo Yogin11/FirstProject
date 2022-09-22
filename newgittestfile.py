2768c2a0f706281b9f3ebc55014e22d7e49156e1
Maxim Epifanov <yogin11@yahoo.com>
2022-09-22 10:25:26 +0300

    Added 2 new files to push them to GitHub

---
+++ newgittestfile.py
@@ -0,0 +1,11 @@
+# открытие файлов
+
+inputfile = "../input.txt"
+
+print (inputfile) 
+
+#path="J:\Japanese Business Department\m.epifanov\Максим\Личн\JapHappy-programming"
+# for j in os.listdir(path):
+print ("Что-то")
+
+# print (os.name)
