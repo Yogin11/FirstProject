a54a55d4f2efa16835a96061f963745d78ea3be5
Max Y <yoyoyo@yahoo.com>
2022-09-23 11:36:04 +0300

    1. Added os.system running of git status in program 2.Changed name and email for the project

--- newgittestfile.py
+++ newgittestfile.py
@@ -1,4 +1,7 @@
 # открытие файлов
+import os
+
+
 
 inputfile = "../input.txt"
 
@@ -7,5 +10,7 @@ print (inputfile)
 #path="J:\Japanese Business Department\m.epifanov\Максим\Личн\JapHappy-programming"
 # for j in os.listdir(path):
 print ("Что-то")
+
+os.system("git status")
 
 # print (os.name)
